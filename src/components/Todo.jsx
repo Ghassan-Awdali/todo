6a595ac02b305a0cd9c1e6b0fdeb0994834a1bd5
G ARUTO <awdalig@gmail.com>
2024-07-18 15:32:48 -0300

    mapped the list in order to display it on todo list

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -20,7 +20,9 @@ export default function Todo() {
         />
         <button type="submit">Add</button>
       </form>
-      {console.log(todos)}
+      {todos.map((item) => (
+        <h3>{item}</h3>
+      ))}
     </div>
   );
 }
